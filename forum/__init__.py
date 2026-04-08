73537e54e63c7d32fbc0723adbb1521802163448
nate <Jamalnathan7@gmail.com>
2026-04-08 16:10:45 -0400

    init file is back

---
+++ forum/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .routes import rt
+
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    # Register the main routes blueprint.
+    app.register_blueprint(rt)
+    # Set globals
+    from .models import db
+    db.init_app(app)
+    
+    with app.app_context():
+        # Create tables on startup so the app can run against a fresh database.
+        db.create_all()
+        return app
+
